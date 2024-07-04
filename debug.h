213f2eb1b5f08e1d96d3bb6a7b9eb03214665c92
Halil Akbaş <2954639@gmail.com>
2024-07-04 14:57:47 +0300

    remove u/f

--- debug.h
+++
@@ -1,9 +0,0 @@
-///
-/// This is for debugging purposes. Don't push it project directory
-///
-//
-
-#include "libft/libft.h"
-void	print_table(char **table);
-void	ft_assert(int expression, int line_no, char *file_name);
-void	print_list(t_list *list);
