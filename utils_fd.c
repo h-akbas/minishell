e616a8c00db92243d8fe24d008f6841c62950272
Halil Akbaş <2954639@gmail.com>
2024-07-12 20:03:28 +0300

    utils

---
+++ utils_fd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_fd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakbas <hakbas@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 17:36:30 by hakbas            #+#    #+#             */
+/*   Updated: 2024/07/12 18:12:24 by hakbas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <errno.h>
+
+void	close_extra_fds(void)
+{
+	int	fd;
+	int	max_fd;
+
+	max_fd = 1024;
+	fd = STDERR_FILENO + 1;
+	while (fd < max_fd)
+	{
+		if (close(fd) == -1 && errno != EBADF)
+		{
+			perror("close");
+			return ;
+		}
+		fd++;
+	}
+}
+
+void	close_all_fds(void)
+{
+	close_extra_fds();
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+}
---
+++ utils_ft_atoll.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_ft_atoll.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakbas <hakbas@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 18:17:46 by hakbas            #+#    #+#             */
+/*   Updated: 2024/07/12 18:25:04 by hakbas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <stdbool.h>
+
+static bool	ft_isspace(char c)
+{
+	return (c == 32 || (c >= 9 && c <= 13));
+}
+
+
+long long	ft_atoll(const char *str)
+{
+	long long	num;
+	int			sign;
+
+	num = 0;
+	sign = 1;
+	while (ft_isspace(*str) && !(*str == '-' || *str == '+'))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = sign * -1;
+		str++;
+	}
+	while (*str && ft_isdigit(*str))
+	{
+		num = (num * 10) + (*str - '0');
+		str++;
+	}
+	return (num * sign);
+}
