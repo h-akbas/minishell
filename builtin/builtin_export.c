8e4a277a21067b551062268ee2cb7e2b246fa3dd
Halil Akbaş <2954639@gmail.com>
2024-07-19 18:42:40 +0300

    alphabetical order at the export cmd

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: hakbas <hakbas@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 19:51:26 by hakbas            #+#    #+#             */
-/*   Updated: 2024/07/19 17:26:20 by hakbas           ###   ########.fr       */
+/*   Updated: 2024/07/19 18:32:34 by hakbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #include <unistd.h>
 
 static int	declare_env(t_env *ms_env);
+/* static t_env	*merge(t_env *a, t_env *b);
+static void		split_list(t_env *src, t_env **front_ref, t_env **back_ref);
+static void		merge_sort(t_env **head_ref); */
 
 int	builtin_export(char **args, t_env **ms_env)
 {
@@ -50,6 +53,7 @@ static int	declare_env(t_env *ms_env)
 {
 	t_env	*tmp;
 
+/* 	merge_sort(&ms_env); */
 	tmp = ms_env;
 	while (tmp)
 	{
@@ -67,3 +71,69 @@ static int	declare_env(t_env *ms_env)
 	}
 	return (EXIT_SUCCESS);
 }
+
+/* static t_env	*merge(t_env *a, t_env *b)
+{
+	t_env	*result;
+
+	result = NULL;
+	if (a == NULL)
+		return (b);
+	else if (b == NULL)
+		return (a);
+	if (ft_strncmp(a->key_pair, b->key_pair, ft_strlen(a->key_pair)) <= 0)
+	{
+		result = a;
+		result->next = merge(a->next, b);
+	}
+	else
+	{
+		result = b;
+		result->next = merge(a, b->next);
+	}
+	return (result);
+}
+
+static void		split_list(t_env *src, t_env **front_ref, t_env **back_ref)
+{
+	t_env	*fast;
+	t_env	*slow;
+
+	if (src == NULL || src->next == NULL)
+	{
+		*front_ref = src;
+		*back_ref = NULL;
+		return ;
+	}
+	slow = src;
+	fast = src->next;
+	while (fast != NULL)
+	{
+		fast = fast->next;
+		if (fast != NULL)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+	*front_ref = src;
+	*back_ref = slow->next;
+	slow->next = NULL;
+	return ;
+}
+
+static void		merge_sort(t_env **head_ref)
+{
+	t_env	*head;
+	t_env	*a;
+	t_env	*b;
+
+	head = *head_ref;
+	if (head == NULL || head->next == NULL)
+		return ;
+	split_list(head, &a, &b);
+	merge_sort(&a);
+	merge_sort(&b);
+	*head_ref = merge(a, b);
+}
+ */
